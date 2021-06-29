8e924ea21e0684768edefa5b47a462b165e02085
GiovaniZan <55863759+GiovaniZan@users.noreply.github.com>
2021-06-29 18:42:46 -0300

    Reestruturação em diretórios
    Implementação da estrutura de diretórios para organização dos programas /exercícios

---
+++ Exercicios/Exercicio5/src/Exercicio5_asm.s
@@ -0,0 +1,92 @@
+        PUBLIC  __iar_program_start
+        PUBLIC  __vector_table
+
+        SECTION .text:CODE:REORDER(1)
+        
+        ;; Keep vector table even if it's not referenced
+        REQUIRE __vector_table
+        
+        THUMB
+        
+__iar_program_start
+        
+main    
+        // MOV R0, #0x0
+        MOVS R0, #0x0    ;
+
+        //MOV R1, #200    ;
+        MOVS R1, #200    ;
+
+        //MOV R2, #0x1234 ;
+        MOV R2, #0x1234 ;
+
+        //MOV R3, #0XFFFFFF00 
+  //      MOV R3, #0XFF00 
+  //      MOVT R3, #0XFFFF
+        MVN R3, #0xff
+        
+        MOV R4, #0XEF00 
+        MOVT R4, #0XABCD
+
+
+        B       main
+
+        ;; Forward declaration of sections.
+        SECTION CSTACK:DATA:NOROOT(3)
+        SECTION .intvec:CODE:NOROOT(2)
+        
+        DATA
+
+__vector_table
+        DCD     sfe(CSTACK)
+        DCD     __iar_program_start
+
+        DCD     NMI_Handler
+        DCD     HardFault_Handler
+        DCD     MemManage_Handler
+        DCD     BusFault_Handler
+        DCD     UsageFault_Handler
+        DCD     0
+        DCD     0
+        DCD     0
+        DCD     0
+        DCD     SVC_Handler
+        DCD     DebugMon_Handler
+        DCD     0
+        DCD     PendSV_Handler
+        DCD     SysTick_Handler
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Default interrupt handlers.
+;;
+
+        PUBWEAK NMI_Handler
+        PUBWEAK HardFault_Handler
+        PUBWEAK MemManage_Handler
+        PUBWEAK BusFault_Handler
+        PUBWEAK UsageFault_Handler
+        PUBWEAK SVC_Handler
+        PUBWEAK DebugMon_Handler
+        PUBWEAK PendSV_Handler
+        PUBWEAK SysTick_Handler
+
+        SECTION .text:CODE:REORDER:NOROOT(1)
+        THUMB
+
+NMI_Handler
+HardFault_Handler
+MemManage_Handler
+BusFault_Handler
+UsageFault_Handler
+SVC_Handler
+DebugMon_Handler
+PendSV_Handler
+SysTick_Handler
+Default_Handler
+__default_handler
+        CALL_GRAPH_ROOT __default_handler, "interrupt"
+        NOCALL __default_handler
+        B __default_handler
+
+        END
